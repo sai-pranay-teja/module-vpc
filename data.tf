321201d202bfbd5f3030f5907a4b00df6f0e955e
saipranayteja <pranayteja.ch@algonox.com>
2023-06-10 16:55:41 +0530

    asdf

--- data.tf
+++ data.tf
@@ -1,3 +1,10 @@
 data "aws_vpc" "default" {
   id = var.default_vpc_id
 }
+
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "igw-1"
+    values = [var.default_vpc_id]
+  }
+}
--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_route_table" "default_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = data.aws_vpc.default.
   }
 
   route {
