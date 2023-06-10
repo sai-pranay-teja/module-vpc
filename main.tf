0f9bab061426f4eaaf6109caa04ccf8130a75ebc
saipranayteja <pranayteja.ch@algonox.com>
2023-06-10 13:47:16 +0530

    asd

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_route_table" "private_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[each.value["name"]].id
   }
 
   route {
