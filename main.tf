94465cca7ebbec52f5628321a808849d44371519
saipranayteja <pranayteja.ch@algonox.com>
2023-06-10 16:18:43 +0530

    dcf

--- main.tf
+++ main.tf
@@ -79,7 +79,8 @@ resource "aws_route_table_association" "default" {
 resource "aws_eip" "eip" {
     #vpc=true
     for_each = var.public_cidr
-    vpc=true
+    domain="vpc"
+
     
     tags = {
         Name = "${var.env}-${each.value["name"]}-Elastic-IP"
